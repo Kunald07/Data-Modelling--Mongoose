86b7496e9be402bdcde4c96956706e284428b804
Kunal Deshmukh <deshmukh1207@gmail.com>
2026-02-27 19:36:10 +0530

    todos commit

---
+++ models/todos/sub_todo.models.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const subTodoSchema = new mongoose.Schema({
+    content: {
+        title: String,
+        required: true,
+    },
+    complete: {
+        type: Boolean,
+        default: false,
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+}, {timestamps: true})
+
+export const SubTodo = mongoose.models("SubTodo", subTodoSchema)
